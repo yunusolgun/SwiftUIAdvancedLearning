58ce9e731142c6db31a8608a69d67249982a4915
Yunus Olgun <y.olgun@gmail.com>
2025-04-15 12:35:30 +0300

    Initial Commit

---
+++ AdvancedLearning/AdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedLearningApp.swift
+//  AdvancedLearning
+//
+//  Created by yunus olgun on 15.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
