e0ec112d0cc9ff7d1c472a86f796570e99f47b24
Yunus Olgun <y.olgun@gmail.com>
2025-04-29 16:17:08 +0300

    Custom Errors and Alerts in SwiftUI | Advanced Learning #28

---
+++ AdvancedLearning/ErrorAlerBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorAlerBootcamp.swift
+//  AdvancedLearning
+//
+//  Created by yunus olgun on 29.04.2025.
+//
+
+import SwiftUI
+
+struct ErrorAlerBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorAlerBootcamp()
+}
