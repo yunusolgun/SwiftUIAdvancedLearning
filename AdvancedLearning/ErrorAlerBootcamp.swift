bc930bbf8d22e243a8334706999cb070f2b75c25
Yunus Olgun <y.olgun@gmail.com>
2025-04-30 11:02:01 +0300

    Custom Errors and Alerts int SwiftUI

--- AdvancedLearning/ErrorAlerBootcamp.swift
+++ AdvancedLearning/ErrorAlerBootcamp.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct ErrorAlerBootcamp: View {
+    
+    @State private var error: Error? = nil
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Click Me") {
+            saveData()
+        }
+        .alert(error?.localizedDescription ?? "Error", isPresented: Binding(value: $error)) {
+            Button("OK") {
+                
+            }
+        }
+        
+
+    }
+    
+    enum MyCustomError: Error, LocalizedError {
+        case noInternetConnection
+        case dataNotFound
+        case urlError(error: Error)
+        
+        var errorDescription: String? {
+            switch self {
+            case .noInternetConnection:
+                return "Please check your internet connection and try again"
+            case .dataNotFound:
+                return "There was an error loading data, please try again later"
+            case .urlError(error: let error):
+                return "Error: \(error.localizedDescription)"
+            }
+        }
+
+    }
+    
+    private func saveData() {
+        
+        let isSuccessfull = false
+        
+        if isSuccessfull {
+            
+        } else {
+//            let myError: Error = URLError(.badURL)
+            let myError: Error = MyCustomError.noInternetConnection
+            
+            error = myError
+
+        }
+        
     }
 }
 
